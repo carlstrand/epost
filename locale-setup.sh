18cfd7acda5fe9c6ea4cf9b1390dbb79310f4967
MC <darwishi@hotmail.com>
2019-02-15 12:57:10 +0100

    ...

--- locale-setup.sh
+++ locale-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sudo apt --quiet --yes --fix install locales
+sudo apt --quiet --yes install locales
 sudo locale-gen sv_SE sv_SE.UTF-8
-sudo dpkg-reconfigure locales 
+sudo update-locale LANG="sv_SE.UTF-8" LANGUAGE="sv_SE"
+sudo dpkg-reconfigure locales
 
+export LANGUAGE="sv_SE.utf8"
 export LC_ALL="sv_SE.UTF-8"
 export LC_CTYPE="sv_SE.UTF-8"
