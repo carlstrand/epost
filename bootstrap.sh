47c629d5c7867c47dd54522a16c492e3e414a7eb
MC <darwishi@hotmail.com>
2019-02-15 01:31:06 +0100

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y software-properties-common 
 
 echo 'alias apl=ansible-playbook' >> ~/.bashrc 
 echo 'alias apl-def=ansible-playbook -i hosts site.yml' >> ~/.bashrc 
@@ -10,7 +14,7 @@ echo "deb http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" | s
 echo "deb http://rspamd.com/apt-stable/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/rspamd.list
 curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
 curl -fsSL https://rspamd.com/apt-stable/gpg.key | sudo apt-key add -
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 
 # - 'deb http://nginx.org/packages/ubuntu/ {{ ansible_distribution_release }} nginx'
 #- 'deb http://rspamd.com/apt-stable/ {{ ansible_distribution_release }} main'
@@ -21,15 +25,15 @@ sudo add-apt-repository ppa:certbot/certbot
 export PYPY3_VERSION=pypy3.5-v7.0.0
 export PYPY3_URL=https://bitbucket.org/pypy/pypy/downloads/$PYPY3_VERSION-linux64.tar.bz2
 export REPO_URL=https://github.com/carlstrand/epost.git
-export LC_ALL="sv_SE.UTF-8"
-export LC_CTYPE="sv_SE.UTF-8"
-export LC_CTYPE="sv_SE.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
 
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y software-properties-common apt-transport-https apt-utils locales virtualenv build-essential git
+sudo apt install -y apt-transport-https apt-utils locales virtualenv build-essential git
 sudo dpkg-reconfigure locales
 
-mkdir ~/ansible && git clone -b dev $REPO_URL ~/ansible
+mkdir ~/ansible && sudo mkdir /opt/$PYPY3_VERSION/ && git clone -b dev $REPO_URL ~/ansible
 
 #############################################
 wget -O pypy3.tar.bz2 $PYPY3_URL
