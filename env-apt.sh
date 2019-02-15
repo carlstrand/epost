cb1320c5c287bc01627974907ccab9f3369a27b2
MC <darwishi@hotmail.com>
2019-02-15 12:53:08 +0100

    ...

--- env-apt.sh
+++ env-apt.sh
@@ -2,10 +2,11 @@
 
 sudo apt update --quiet --yes && sudo apt upgrade --quiet --yes
 
-sudo apt --quiet --yes --fix install \
+sudo apt --quiet --yes install \
         software-properties-common \
         apt-transport-https \
         apt-utils \
+        libtinfo-dev \
         virtualenv \
         build-essential \
         git \
--- locale-setup.sh
+++ locale-setup.sh
@@ -2,3 +2,6 @@
 sudo apt --quiet --yes --fix install locales
 sudo locale-gen sv_SE sv_SE.UTF-8
 sudo dpkg-reconfigure locales 
+
+export LC_ALL="sv_SE.UTF-8"
+export LC_CTYPE="sv_SE.UTF-8"
