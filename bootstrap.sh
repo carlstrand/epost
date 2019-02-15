5f4dfeaf7adf1796c858eb9559dcde9a192f5c4b
MC <darwishi@hotmail.com>
2019-02-15 02:29:10 +0100

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,34 +23,34 @@ sudo add-apt-repository ppa:certbot/certbot
 
 
 export PYPY3_VERSION=pypy3.5-v7.0.0
-export PYPY3_URL=https://bitbucket.org/pypy/pypy/downloads/$PYPY3_VERSION-linux64.tar.bz2
+export PYPY3_URL=https://bitbucket.org/pypy/pypy/downloads/$(PYPY3_VERSION)-linux64.tar.bz2
 export REPO_URL=https://github.com/carlstrand/epost.git
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 
 sudo apt update -y -q && sudo apt upgrade -y -q
-sudo apt install -y apt-transport-https apt-utils locales virtualenv build-essential git
+sudo apt install -y apt-transport-https apt-utils locales virtualenv build-essential git libssl-dev
 sudo dpkg-reconfigure locales
 
 mkdir ~/ansible
-mkdir ~/local
-mkdir ~/local/$PYPY3_VERSION
+mkdir -p ~/.local/$PYPY3_VERSION
 
 git clone -b dev $REPO_URL ~/ansible
 
 #############################################
 wget -O pypy3.tar.bz2 $PYPY3_URL
-tar -xf pypy3.tar.bz2 -C ~/local/$PYPY3_VERSION
+tar -xf pypy3.tar.bz2 -C ~/.local/$PYPY3_VERSION --strip-components=1
 rm -f pypy3.tar.bz2 
 
 cd ~/ansible
-sudo virtualenv --python=~/.local/$PYPY3_VERSION/bin/pypy3 ~/ansible
-source ~/ansible/bin/activate
-sudo pypy3 -m ensurepip
-sudo pypy3 -m pip install --upgrade pip
-sudo pip3 install setuptools wheel libpq-dev ffi-dev psycopg2cffi pycrypto pycparser paramiko 
-sudo pip3 install ansible ansible-lint ansible-review
-sudo pypy3 psqlcom.py
+sudo virtualenv -p ~/.local/$PYPY3_VERSION/bin/pypy3 .
+sudo source ./bin/activate
+sudo ~/.local/$PYPY3_VERSION/bin/pypy3 -m ensurepip
+sudo ~/.local/$PYPY3_VERSION/bin/pypy3 -m pip install --upgrade pip --quiet
+#sudo pip3 install psycopg2cffi pycrypto pycparser paramiko 
+sudo ~/.local/$PYPY3_VERSION/bin/pip3 install ansible ansible-lint ansible-review
 
+
+sudo pypy3 psqlcom.py
 #sudo apl-test -vvv
